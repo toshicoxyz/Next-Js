c4875649bf622cfdf163c2a6fc9416e5e6e521da
Toshico <colcapedro@gmail.com>
2023-06-29 23:03:17 -0500

    Poder subir mp3 sera posible ?

--- app-next/src/app/page.tsx
+++ app-next/src/app/page.tsx
@@ -1,42 +1,42 @@
 'use client'
 
-// import Image from 'next/image'
-import { useState } from 'react'
-
-// import Mp3Player from './Mp3Player'
+import { useEffect, useRef } from 'react'
 
 export default function Home() {
-  const [state, setState] = useState(0)
+  const audioRef = useRef<HTMLAudioElement | null>(null)
 
-  const audio = new Audio()
-  audio.src = `Confident.mp3`
-  audio.controls = false
-  audio.loop = false
-  audio.autoplay = false
+  useEffect(() => {
+    audioRef.current = new Audio('Confident.mp3')
+    audioRef.current.controls = false
+    audioRef.current.loop = false
+  }, [])
+
+  const playAudio = () => {
+    if (audioRef.current) {
+      audioRef.current.play()
+    }
+  }
+
+  const pauseAudio = () => {
+    if (audioRef.current) {
+      audioRef.current.pause()
+    }
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
-      <input
-        type="file"
-        className="block w-full text-sm text-slate-500
-        file:mr-4 file:py-2 file:px-4
-        file:rounded-full file:border-0
-        file:text-sm file:font-semibold
-        file:bg-violet-50 file:text-violet-700
-        hover:file:bg-violet-100"
-      />
       <button
-        className="block w-full text-sm text-slate-500
-        file:mr-4 file:py-2 file:px-4
-        file:rounded-full 
-        file:text-sm file:font-semibold
-        file:bg-violet-50 file:text-violet-700
-        hover:file:bg-violet-100"
-        onClick={() => audio.play()}
+        className="block w-full text-sm text-slate-500"
+        onClick={playAudio}
       >
         play
       </button>
-      {/* <Mp3Player></Mp3Player> */}
+      <button
+        className="block w-full text-sm text-slate-500"
+        onClick={pauseAudio}
+      >
+        Pause
+      </button>
     </main>
   )
 }
