2544b90574549d591d7914150b3327c3f5cfaa10
Toshico <colcapedro@gmail.com>
2023-07-01 23:34:30 -0500

    Entendiendo variable session de Clerk

--- app-next/src/app/(root)/page.tsx
+++ app-next/src/app/(root)/page.tsx
@@ -1,9 +1,17 @@
-import { UserButton } from '@clerk/nextjs'
+'use client'
+
+import { UserButton, useClerk } from '@clerk/nextjs'
 
 const SetupPage = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { session } = useClerk()
+
+  // Verificar si no hay una sesión activa y redirigir al inicio de sesión
+  console.log(session)
+  
   return (
     <div className="p-4">
-      <button className=''>HOLA</button>
+      <button className="">HOLA</button>
       <UserButton afterSignOutUrl="/" />
     </div>
   )
