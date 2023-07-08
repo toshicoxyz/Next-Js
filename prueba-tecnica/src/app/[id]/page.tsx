6fa1bd8f7b07f1e9aae5918ff49f12a5e750a90e
Toshico <colcapedro@gmail.com>
2023-07-07 19:59:36 -0500

    Poniendo diseño a la prueba tecnica segun lo que se pidio

--- prueba-tecnica/src/app/[id]/page.tsx
+++ prueba-tecnica/src/app/[id]/page.tsx
@@ -5,18 +5,30 @@ import { getIdUser } from '@/services/crud'
 import { useEffect, useState } from 'react'
 import { User } from '@/models/model'
 import db from '@/firebase/config'
-import { CircularProgress, Avatar } from '@mui/material'
+import {
+  CircularProgress,
+  Card,
+  Link,
+  Grid,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  CardMedia,
+  Snackbar,
+} from '@mui/material'
 
 const NuevaPagina = ({ params }: { params: { id: string } }) => {
-  const [city, setCity] = useState<User | null>(null)
+  const [user, setUser] = useState<User | null>(null)
   const [loading, setLoading] = useState(true)
+  const [snackbarOpen, setSnackbarOpen] = useState(false)
 
   useEffect(() => {
     if (params.id) {
       const getId = async (id: string) => {
         try {
           const data = (await getIdUser(db, id)) ?? null
-          setCity(data)
+          setUser(data)
           setLoading(false)
         } catch (error) {
           console.error('Error fetching data: ', error)
@@ -25,7 +37,7 @@ const NuevaPagina = ({ params }: { params: { id: string } }) => {
 
       getId(params.id)
     } else {
-      setCity(null)
+      setUser(null)
       setLoading(false)
     }
   }, [params.id])
@@ -38,7 +50,7 @@ const NuevaPagina = ({ params }: { params: { id: string } }) => {
     )
   }
 
-  if (!city) {
+  if (!user) {
     return (
       <div className="flex items-center justify-center h-screen content-center">
         No se encontro el archivo
@@ -48,28 +60,89 @@ const NuevaPagina = ({ params }: { params: { id: string } }) => {
 
   return (
     <div className="flex items-center justify-center h-screen content-center">
-      {city && (
-        <div className="border-4 border-gray-500/25 p-4 rounded-2xl text-black bg-transparent">
-          <Avatar
-            className="rounded-3xl m-auto"
-            src={city?.image}
-            alt={city.first}
-            sx={{ width: 200, height: 200 }}
-          />
-          <p>
-            <b>ID:</b> {city?.id}
-          </p>
-          <p>
-            <b>Nombre:</b> {city?.first}
-          </p>
-          <p>
-            <b>Apellido:</b> {city?.last}
-          </p>
-          <p>
-            <b>Edad:</b> {city?.born}
-          </p>
-        </div>
+      {user && (
+        <Card
+          key={user.id}
+          sx={{
+            margin: 2,
+          }}
+        >
+          <Grid container sx={{ alignItems: 'center' }}>
+            <Grid item xs={12} sm={6}>
+              <CardContent
+                sx={{
+                  maxWidth: '1200px',
+                  padding: 5,
+                  height: '100%',
+                }}
+              >
+                <Typography gutterBottom variant="h5" component="div">
+                  {user.first}
+                </Typography>
+                <Typography variant="body1" color="text.secondary">
+                  Last: {user.last}
+                </Typography>
+                <Typography variant="body1" color="text.secondary">
+                  Born: {user.born}
+                </Typography>
+                <Typography variant="body1" color="text.secondary">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industrys
+                  standard dummy text ever since the 1500s, when an unknown
+                </Typography>
+                <CardActions>
+                  <Button
+                    size="small"
+                    sx={{
+                      padding: 2,
+                      margin: 'auto',
+                      '&:hover': {
+                        backgroundColor: '#007bff',
+                        color: 'white',
+                        cursor: 'pointer',
+                        transition: 'background-color 0.3s ease',
+                      },
+                    }}
+                    onClick={e => {
+                      e.preventDefault()
+                      const link = `${window.location.origin}/${user.id}`
+                      navigator.clipboard
+                        .writeText(link)
+                        .then(() => {
+                          setSnackbarOpen(true)
+                          // Puedes mostrar un mensaje de éxito aquí si lo deseas
+                        })
+                        .catch(error => {
+                          console.error('Error copying link:', error)
+                          // Puedes mostrar un mensaje de error aquí si lo deseas
+                        })
+                    }}
+                  >
+                    Share
+                  </Button>
+                </CardActions>
+              </CardContent>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <CardMedia
+                sx={{ width: '100%', paddingTop: '100%' }}
+                image={
+                  user?.image
+                    ? user.image
+                    : 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+                }
+                title={user.first}
+              />
+            </Grid>
+          </Grid>
+        </Card>
       )}
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={3000}
+        onClose={() => setSnackbarOpen(false)}
+        message="Link copeado!"
+      />
     </div>
   )
 }
--- prueba-tecnica/src/components/Client/DataList.tsx
+++ prueba-tecnica/src/components/Client/DataList.tsx
@@ -1,13 +1,18 @@
+/* eslint-disable react/jsx-no-undef */
 /* eslint-disable @next/next/no-img-element */
 'use client'
 
 import { useEffect, useState } from 'react'
 import {
-  List,
-  ListItem,
-  ListItemText,
   CircularProgress,
-  Avatar,
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  Grid,
+  Snackbar,
 } from '@mui/material'
 import { Firestore } from 'firebase/firestore'
 import { getAllUsers, getAllUsersUp } from '@/services/crud'
@@ -21,6 +26,7 @@ interface DataListProps {
 export default function DataList({ db }: DataListProps) {
   const [dataList, setDataList] = useState<User[]>([])
   const [loading, setLoading] = useState(true)
+  const [snackbarOpen, setSnackbarOpen] = useState(false)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -46,24 +52,117 @@ export default function DataList({ db }: DataListProps) {
   }
 
   return (
-    <List className="w-full">
-      {dataList.map(user => (
-        <ListItem key={user.id}>
-          <Avatar
-            className="rounded-full m-3"
-            src={user?.image}
-            alt={user.first}
-            sx={{ width: 50, height: 50 }}
-          />
-          <ListItemText primary={user.first} secondary={user.last} />
-          <Link
-            className="rounded-2xl p-5 border border-slate-300 hover:border-slate-400"
-            href={`/${user.id}`}
-          >
-            Ver
+    <>
+      {dataList.map(item => (
+        <Card
+          key={item.id}
+          sx={{
+            margin: 2,
+            '&:hover': {
+              backgroundColor: 'rgba(0, 0, 0, 0.1)',
+              cursor: 'pointer',
+              transition: 'background-color 0.3s ease',
+            },
+          }}
+        >
+          <Link href={`/${item.id}`}>
+            <Grid container sx={{ alignItems: 'center' }}>
+              <Grid item xs={12} sm={6}>
+                <CardContent
+                  sx={{
+                    maxWidth: '1200px',
+                    padding: 5,
+                    height: '100%',
+                  }}
+                >
+                  <Typography gutterBottom variant="h5" component="div">
+                    {item.first}
+                  </Typography>
+                  <Typography variant="body1" color="text.secondary">
+                    Last: {item.last}
+                  </Typography>
+                  <Typography variant="body1" color="text.secondary">
+                    Born: {item.born}
+                  </Typography>
+                  <Typography variant="body1" color="text.secondary">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industrys
+                    standard dummy text ever since the 1500s, when an unknown
+                  </Typography>
+                  <CardActions>
+                    <Button
+                      size="small"
+                      sx={{
+                        padding: 2,
+                        margin: 'auto',
+                        '&:hover': {
+                          backgroundColor: '#007bff',
+                          color: 'white',
+                          cursor: 'pointer',
+                          transition: 'background-color 0.3s ease',
+                        },
+                      }}
+                      onClick={e => {
+                        e.preventDefault()
+                        const link = `${window.location.origin}/${item.id}`
+                        navigator.clipboard
+                          .writeText(link)
+                          .then(() => {
+                            setSnackbarOpen(true)
+                            // Puedes mostrar un mensaje de éxito aquí si lo deseas
+                          })
+                          .catch(error => {
+                            console.error('Error copying link:', error)
+                            // Puedes mostrar un mensaje de error aquí si lo deseas
+                          })
+                      }}
+                    >
+                      Share
+                    </Button>
+                  </CardActions>
+                </CardContent>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <CardMedia
+                  sx={{ width: '100%', paddingTop: '100%' }}
+                  image={
+                    item?.image
+                      ? item.image
+                      : 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+                  }
+                  title={item.first}
+                />
+              </Grid>
+            </Grid>
           </Link>
-        </ListItem>
+        </Card>
       ))}
-    </List>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={3000}
+        onClose={() => setSnackbarOpen(false)}
+        message="Link copeado!"
+      />
+    </>
   )
 }
+
+// <List className="w-full">
+//   {dataList.map(user => (
+//     <ListItem className="border-2 " key={user.id}>
+//       <ListItemText primary={user.first} secondary={user.last} />
+//       <Link
+//         className="rounded-2xl p-5 border border-slate-300 hover:border-slate-400"
+//         href={`/${user.id}`}
+//       >
+//         Ver
+//       </Link>
+//       <Avatar
+//         className="rounded-full m-3"
+//         src={user?.image}
+//         alt={user.first}
+//         sx={{ width: 50, height: 50 }}
+//       />
+//     </ListItem>
+//   ))}
+// </List>
